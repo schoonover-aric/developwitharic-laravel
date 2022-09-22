ce75d418966a409c3abefd1a59aaa2e860f91435
Aric Schoonover <aschoonover@hartinc.com>
2022-09-21 22:02:10 -0400

    created comments markup, database setup

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
             <x-posts-grid :posts="$posts" />
-
+        {{-- //show pagination --}}
         {{ $posts->links() }}
 
         @else
