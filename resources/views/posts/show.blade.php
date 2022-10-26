3d76cacc83d68a168d3d47409ab1e912dfec6842
Aric Schoonover <schoonover.aric@gmail.com>
2022-10-26 13:13:05 -0400

    changed '{{}}' to '{git add .git add .}' to process html

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -47,7 +47,7 @@
                         {{ $post->title }}
                     </h1>
 
-                    <div class="space-y-4 lg:text-lg leading-loose">{{ $post->body }}</div>
+                    <div class="space-y-4 lg:text-lg leading-loose">{!! $post->body !!}</div>
                 </div>
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
                     @include('posts._add-comment-form')
